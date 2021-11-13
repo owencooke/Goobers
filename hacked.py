34ed06335ea254f9bb9b4638b8dff6e8d41305fd
Alina <iunusova@ualberta.ca>
2021-11-13 13:32:46 -0700

    Update hacked.py

--- hacked.py
+++ hacked.py
@@ -7,3 +7,8 @@ Contributors: Zamam Moeez, Jared Drueco, Aron Gu, Owen Cooke, Alina Iunusova
 # gpa calculator idk
 # Predictions based on mathematical models
 # perhaps load data as a text file, reading it shouldnt be too difficult
+
+import pandas
+
+price_data = pandas.read_excel('oil_prices.xlsx', sheet_name = 'Sheet1')
+
