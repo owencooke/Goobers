29b8eaa726c08bdb81ead83cfc96a7f1a0f98214
Alina <iunusova@ualberta.ca>
2021-11-13 15:50:08 -0700

    Update testingapi.py

--- testingapi.py
+++ testingapi.py
@@ -4,12 +4,12 @@ import yfinance as yf
 
 sleep(1)
 
+#get stock code and period
 stock = str(input('Please name the stock code:  '))
 period = str(input('What time interval do you want the prediction to be based on? (1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max):  '))
 
-stock_info = yf.Ticker(stock)
-historical = stock_info.history(period="period")
+stock_info = yf.Ticker(stock)   #retrieves stock info
+historical = stock_info.history(period=period)  #historical data for a given period
 
-#print(historical)
+print(historical)
 
-n = historical[-1]
