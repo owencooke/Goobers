e1028591784918f83bde2a12aee398c394342cf0
Jared Drueco <drueco03@gmail.com>
2021-11-13 19:23:20 -0700

    just added a legend if we want to keep both plots along with trendline

--- testingapi.py
+++ testingapi.py
@@ -10,17 +10,17 @@ import matplotlib.pyplot as plt
 sleep(1)
 
 #get stock code and period
-stock = str(input('Please name the stock code:  '))
-period = str(input('What time interval do you want the prediction to be based on? (1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max):  '))
+# stock = str(input('Please name the stock code:  '))
+# period = str(input('What time interval do you want the prediction to be based on? (1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max):  '))
 
-stock_info = yf.Ticker(stock)   #retrieves stock info
-historical = stock_info.history(period=period)  #historical data for a given period
+stock_info = yf.Ticker('CL=F')   #retrieves stock info
+historical = stock_info.history(period='10d')  #historical data for a given period
 
-print("Maximum Closing Price: {0}".format(max(historical.Close)))
-print("Minimum Closing Price: {0}".format(min(historical.Close)))
+# print("Maximum Closing Price: {0}".format(max(historical.Close)))
+# print("Minimum Closing Price: {0}".format(min(historical.Close)))
 
-print(historical)
-print(str(type(historical)))
+# print(historical)
+# print(str(type(historical)))
 
 #price_august282000 = historical.Close["2000-08-28"]
 
@@ -40,8 +40,9 @@ print(lollist)
 
 plt.xlabel("Days")
 plt.ylabel("Stock Price")
-plt.plot(lollist)
-plt.plot(higlist)
+plt.plot(higlist, "-r", label='Highs')
+plt.plot(lollist, "-b", label='lows')
+plt.legend()
 plt.show()
 
 
