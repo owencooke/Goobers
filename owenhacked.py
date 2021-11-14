895954a1e9d53cccd69de397e1864463ff689e9e
Zamam-Moeez <zmoeez@outlook.com>
2021-11-14 02:12:38 -0700

    hope this shit works

--- owenhacked.py
+++ owenhacked.py
@@ -32,52 +32,90 @@ closelist = close.to_numpy()
 #print(closelist)
 
 # Simple Moving Average Based on User-Input
-mva = int(input('What length of moving average would you like to apply? Enter an integer: '))
-simple_mvas = []
-exp_mvas = []
-smoothing = 2/(1+mva)  # source for EMA formula: https://www.investopedia.com/terms/e/ema.asp
+mva1 = int(input('What length of moving average would you like to apply? Enter an integer: '))
+mva2 = int(input('Please enter a shorter moving average length, enter an integer: '))
+simple_mvas1 = []
+exp_mvas1 = []
+simple_mvas2 =[]
+exp_mvas2 =[]
+smoothing1 = 2/(1+mva1)  # source for EMA formula: https://www.investopedia.com/terms/e/ema.asp
+smoothing2 = 2/(1+mva2)
 
-for i in range(mva, len(closelist)+1):
-	simple_mvas.append(sum(closelist[(i-mva):i])/mva)
+for i in range(mva1, len(closelist)+1):
+	simple_mvas1.append(sum(closelist[(i-mva1):i])/mva1)
+
+for i in range(mva2, len(closelist)+1):
+	simple_mvas2.append(sum(closelist[(i-mva2):i])/mva2)
 
 # Exponential Moving Average Based on User-Input
-exp_mvas.append(closelist[0])
+exp_mvas1.append(closelist[0])
 for i in range(1, len(closelist)):
-		exp_mvas.append(closelist[i]*smoothing + exp_mvas[i-1]*(1-smoothing))  # see above source
-        
-print(len(exp_mvas))
-print(len(simple_mvas))
+		exp_mvas1.append(closelist[i]*smoothing1 + exp_mvas1[i-1]*(1-smoothing1))  # see above source
 
+exp_mvas2.append(closelist[0])
+for i in range(1, len(closelist)):
+		exp_mvas2.append(closelist[i]*smoothing2 + exp_mvas2[i-1]*(1-smoothing2))
+
+print(len(exp_mvas1))
+print(len(exp_mvas2))
+print(len(simple_mvas1))
+print(len(simple_mvas2))
 inter1 = [] # list of tuples to contain days b/w which an intersection occurs such that exp_mvas is increasing
 inter2 = [] # list of tuples to contain days b/w which an intersection occurs such that simple_mvas is increasing
+
+intra1 = []
+intra2 = []
 
 #print(exp_mvas)
 #print(simple_mvas)
 
 #############################################################################################################
 
-g = [len(exp_mvas),len(simple_mvas)]
+g = [len(simple_mvas2),len(simple_mvas1)]
+k = [len(exp_mvas2),len(exp_mvas1)]
 
 lowg = g.sort()
+lowk = k.sort()
 print(g)
+print(k)
 
 for i in range(g[0]-1):
-     if simple_mvas[i]>exp_mvas[i] and simple_mvas[i+1]<exp_mvas[i+1]:
+     if simple_mvas1[i]>simple_mvas2[i] and simple_mvas1[i+1]<simple_mvas2[i+1]:
          inter1.append((i,i+1))
-     if simple_mvas[i]<exp_mvas[i] and simple_mvas[i+1]>exp_mvas[i+1]:
+     if simple_mvas1[i]<simple_mvas2[i] and simple_mvas1[i+1]>simple_mvas2[i+1]:
          inter2.append((i,i+1))
+
+for i in range(k[0]-1):
+     if exp_mvas1[i]>exp_mvas2[i] and exp_mvas1[i+1]<exp_mvas2[i+1]:
+         intra1.append((i,i+1))
+     if exp_mvas1[i]<exp_mvas2[i] and exp_mvas1[i+1]>exp_mvas2[i+1]:
+         intra2.append((i,i+1))
+
+
+print("simple intersections, lower interval increasing")
 print(inter1)
+print("simple intersentions, larger interval increasing")
 print(inter2)
+print("exp intersections, lower interval increasing")
+print(intra1)
+print("exp intersentions, larger interval increasing")
+print(intra2)
+
+
 
 
 days = list(range(0, len(closelist)))
-plt.plot(days, closelist, label='Closing Prices')
-plt.plot(simple_mvas, 'r-', label='Simple ' + str(mva) + '-Day Moving Average')
-plt.plot(exp_mvas, 'g-', label='Exponential ' + str(mva) + '-Day Moving Average')
+plt.plot(days, closelist,'k-', label='Closing Prices')
+plt.plot(simple_mvas1, 'r--', label='Simple ' + str(mva1) + '-Day Moving Average')
+plt.plot(exp_mvas1, 'g-', label='Exponential ' + str(mva1) + '-Day Moving Average')
+plt.plot(simple_mvas2, 'b--', label='Simple ' + str(mva2) + '-Day Moving Average')
+plt.plot(exp_mvas2, 'y-', label='Exponential ' + str(mva2) + '-Day Moving Average')
 plt.ylabel('Price ($)')
 plt.xlabel('Days')
 plt.legend()
 plt.title(stock + ' Stock - Period: ' + period + ' (source: YahooFinance)')
 
 plt.show()
+
+
 
